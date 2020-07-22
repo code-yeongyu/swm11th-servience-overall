474d1ca75c811041b26109ab1eba74fddd82588c
YeonGyu Kim <code.yeon.gyu@gmail.com>
2020-07-22 10:06:22 +0900

    Refactor registration.dart to use wrapped outlineBorderTextFormField instead of using raw TextFormField

--- app/src/servience/lib/page/registration.dart
+++ app/src/servience/lib/page/registration.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:servience/components/button.dart';
+import 'package:servience/components/textField.dart';
 
 class RegistrationPage extends StatefulWidget {
   RegistrationPage({Key key, this.title}) : super(key: key);
@@ -23,38 +25,18 @@ class _RegistrationPageState extends State<RegistrationPage> {
                 child: Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: <Widget>[
-                      TextFormField(
-                          decoration: const InputDecoration(hintText: 'Username', border: const OutlineInputBorder())
-                      ),
+                      outlineBorderTextFormField("Username", false),
                       Padding(padding: const EdgeInsets.all(10)),
-                      TextFormField(
-                          obscureText: true,
-                          decoration: const InputDecoration(hintText: 'Password', border: const OutlineInputBorder())
-                      ),
+                      outlineBorderTextFormField("Password", true),
                       Padding(padding: const EdgeInsets.all(10)),
-                      TextFormField(
-                          decoration: const InputDecoration(hintText: 'Nickname', border: const OutlineInputBorder())
-                      )
+                      outlineBorderTextFormField("Nickname", false),
                     ]
                 )
             )
             ),
             Padding(
               padding: const EdgeInsets.symmetric(vertical: 16.0),
-              child: FlatButton(
-                color: const Color(0xFF019CB7),
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(18.0),
-                ),
-                onPressed: () {
-                  // Validate will return true if the form is valid, or false if
-                  // the form is invalid.
-                  /*if (_formKey.currentState.validate()) {
-                    // Process data.
-                  }*/
-                },
-                child: Text('Submit', style: TextStyle(color: Colors.white)),
-              ),
+              child: roundedButton(Colors.blue[400], () {}, "Submit"),
             ),
           ],
         ),
