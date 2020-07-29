a4a6f13fb6db22ce037942dcace66b2af4175b2f
ssk807 <ssk807@naver.com>
2020-07-29 17:01:35 +0900

    Update move from current position for estimating pose

--- robot/src/sending_goals/sending_goals.cpp
+++ robot/src/sending_goals/sending_goals.cpp
@@ -6,7 +6,10 @@
 #include <string>
 #include <ctime>
 #include <queue>
+#include <signal.h>
+#include <unistd.h>
 using namespace std;
+
 
 pair<double,double> Table[10];
 int main(int argc, char** argv)
@@ -30,7 +33,20 @@ int main(int argc, char** argv)
 	bool startTrigger = false;	// triger for start
 	bool goalReach = false;		// to tell whether robot is arrived at goal
 	queue<int> Table_queue;		// Table index for serving
+	int lastest_Table_idx = -1;
 
+	// 2d Estimate Pose
+	system("rostopic pub -1 /initialpose geometry_msgs/PoseWithCovarianceStamped \"header:\n  seq: 0\n  stamp:\n    secs: 0\n    nsecs: 0\n  frame_id: 'map'\npose:\n  pose:\n    position: {x: -2.019, y: -0.329, z: 0.0}\n    orientation: {x: 0.0, y: 0.0, z: -0.0135, w: 0.999}\n  covariance: [0.25, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.25, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,\n    0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,\n    0.0, 0.0, 0.0, 0.0, 0.0, 0.06853891945200942]\"");	
+
+	// spin on the right
+	system("rostopic pub -1 /cmd_vel geometry_msgs/Twist \"linear:\n  x: 0.0\n  y: 0.0\n  z: 0.0\nangular:\n  x: 0.0\n  y: 0.0\n  z: 2.0\"");
+	sleep(3);
+	system("rostopic pub -1 /cmd_vel geometry_msgs/Twist \"linear:\n  x: 0.0\n  y: 0.0\n  z: 0.0\nangular:\n  x: 0.0\n  y: 0.0\n  z: 0.0\"");
+
+	// spin on the left
+	system("rostopic pub -1 /cmd_vel geometry_msgs/Twist \"linear:\n  x: 0.0\n  y: 0.0\n  z: 0.0\nangular:\n  x: 0.0\n  y: 0.0\n  z: -2.0\"");
+	sleep(3);
+	system("rostopic pub -1 /cmd_vel geometry_msgs/Twist \"linear:\n  x: 0.0\n  y: 0.0\n  z: 0.0\nangular:\n  x: 0.0\n  y: 0.0\n  z: 0.0\"");
 	while(true)
 	{
 		int Table_idx = rand() % 9;
@@ -41,6 +57,8 @@ int main(int argc, char** argv)
 			while(!Table_queue.empty())		// robot is moving untli job is empty
 			{
 				int tableIdx = Table_queue.front(); Table_queue.pop();
+				if(lastest_Table_idx == tableIdx)continue;
+				lastest_Table_idx = tableIdx;
 				goalReach = false;
 				string x = to_string(Table[tableIdx].first);	x.append("\n");
 				string y = to_string(Table[tableIdx].second);	y.append("\n");
