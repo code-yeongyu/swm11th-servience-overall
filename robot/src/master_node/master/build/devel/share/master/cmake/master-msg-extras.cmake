a7dc440a4f8e08e561c21f98000116dabd05464e
ssk807 <ssk807@naver.com>
2020-10-03 15:21:19 +0900

    Update setting master node

---
+++ robot/src/master_node/master/build/devel/share/master/cmake/master-msg-extras.cmake
@@ -0,0 +1,2 @@
+set(master_MESSAGE_FILES "/home/ssk807/catkin_ws/src/master/msg/Pair.msg;/home/ssk807/catkin_ws/src/master/msg/CupNumber.msg")
+set(master_SERVICE_FILES "")
---
+++ robot/src/master_node/master/build/devel/share/master/cmake/master-msg-paths.cmake
@@ -0,0 +1,4 @@
+# generated from genmsg/cmake/pkg-msg-paths.cmake.develspace.in
+
+set(master_MSG_INCLUDE_DIRS "/home/ssk807/catkin_ws/src/master/msg")
+set(master_MSG_DEPENDENCIES std_msgs)
