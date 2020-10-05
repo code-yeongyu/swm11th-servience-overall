98f9fd5bf78a023cdb4c735a57fb4c2e32d7cd6f
ssk807 <ssk807@naver.com>
2020-10-05 17:20:27 +0900

    Modify subscribe topic function in master_node

--- robot/src/master_node/master/src/master.cpp
+++ robot/src/master_node/master/src/master.cpp
@@ -28,108 +28,126 @@ void RecieveOrderList();
 
 int main(int argc, char **argv)
 {
-    ros::init(argc,argv,"master");
-    ros::NodeHandle nh;
+	ros::init(argc,argv,"master");
+	ros::NodeHandle nh;
 
-    ros::Subscriber cupnum_sub = nh.subscribe("cupnum_msg",100,SendCupNumber);
-    ros::Publisher table_pub = nh.advertise<master::CupNumber>("table_msg",100);
-    
-    websocket_client client;
-    client.connect(U("ws://3.35.95.187:3001")).then([](){});
-    
-    client.receive().then([=](websocket_incoming_message msg){
-        master::CupNumber cupNumberMsg;
-        master::Pair pairMsg;
-        cupNumberMsg.data.push_back(pairMsg);
-        table_pub.publish(cupNumberMsg);
-        return msg.extract_string();
-    }).then([](string body){
-        cout<<body<<endl;
-    });
-    
-    cout<<"Master Start"<<'\n';
-    ros::spin();
+	ros::Subscriber cupnum_on_sub = nh.subscribe("cup_num_on",100,SendCupNumberOn);
+	ros::Subscriber cupnum_off_sub = nh.subscribe("cup_num_off",100,SendCupNumberOff);
+	ros::Publisher table_pub = nh.advertise<master::CupNumber>("trigger",100);
 
-    RecieveOrderList();
-    
-    return 0;
+	websocket_client client;
+	client.connect(U("ws://3.35.95.187:3001")).then([](){});
+
+	client.receive().then([=](websocket_incoming_message msg){
+			master::CupNumber cupNumberMsg;
+			master::Pair pairMsg;
+			cupNumberMsg.data.push_back(pairMsg);
+			table_pub.publish(cupNumberMsg);
+			return msg.extract_string();
+			}).then([](string body){
+				cout<<body<<endl;
+				});
+
+	cout<<"Master Start"<<'\n';
+	ros::spin();
+
+	RecieveOrderList();
+
+	return 0;
 }
 
-void SendCupNumber(const master::Pair& _num)
+void SendCupNumberOn(const master::Int32& _num)
 {
-    int num_a = _num.a;
-    int num_b = _num.b;
-    http_client client(U("http://3.35.95.187:3000"));
-    //ROS_INFO("recieve cup_num : %d",num_b);
-    if(num_a==-1)
-	{
-        http_client tmp(U("http://3.35.95.187:3000/order"));
-        client = tmp;
-    }
-    else
-    {
-        http_client tmp(U("http://3.35.95.187:3000/order"));
-        client = tmp;
-    }
-    
-    json::value postData;
-    postData["cup_number"] = json::value::number(num_b);
+	int num = _num;
+	http_client client(U("http://3.35.95.187:3000"));
+	ROS_INFO("recieve cup_num_on : %d",num);
 
-    client.request(methods::POST).then([=](const http_response& r){
-		cout << U("STATUS : ") << r.status_code() << endl;
-		cout << "content-type : " << r.headers().content_type() << endl;
-        r.extract_json().then([](json::value v){
-            auto i = v.as_object().at(U("order")).as_array();
-            for(auto I : i)
-                cout<<I.as_string()<<'\n';
-        
-        });
-    }).wait();
+	json::value postData;
+	postData["cup_number"] = json::value::number(num);
 
-    // client.request(methods::POST,U("/"),postData).then([](const http_response& response){
-    //     return response.extract_json();
-    // });
-    
-    ROS_INFO("finish sending cup number to server");
+	client.request(methods::POST).then([=](const http_response& r){
+			cout << U("STATUS : ") << r.status_code() << endl;
+			cout << "content-type : " << r.headers().content_type() << endl;
+			r.extract_json().then([](json::value v){
+					auto i = v.as_object().at(U("order")).as_array();
+					for(auto I : i)
+					cout<<I.as_string()<<'\n';
+
+					});
+			}).wait();
+
+	// client.request(methods::POST,U("/"),postData).then([](const http_response& response){
+	//     return response.extract_json();
+	// });
+
+	ROS_INFO("finish sending cup number to server");
+}
+
+void SendCupNumberOff(const master::Int32& _num)
+{
+	int num = _num;
+	http_client client(U("http://3.35.95.187:3000"));
+	ROS_INFO("recieve cup_num_off : %d",num);
+
+	json::value postData;
+	postData["cup_number"] = json::value::number(num);
+
+	client.request(methods::POST).then([=](const http_response& r){
+			cout << U("STATUS : ") << r.status_code() << endl;
+			cout << "content-type : " << r.headers().content_type() << endl;
+			r.extract_json().then([](json::value v){
+					auto i = v.as_object().at(U("order")).as_array();
+					for(auto I : i)
+					cout<<I.as_string()<<'\n';
+
+					});
+			}).wait();
+
+	// client.request(methods::POST,U("/"),postData).then([](const http_response& response){
+	//     return response.extract_json();
+	// });
+
+	ROS_INFO("finish sending cup number to server");
+
 }
 
 void GetJson()
 {
 	http_client client(U("http://3.35.95.187:3000/order"));
- 
+
 	http_request req(methods::POST);
-    
+
 	client.request(req).then([=](http_response r){
-		cout << U("STATUS : ") << r.status_code() << endl;
-		cout << "content-type : " << r.headers().content_type() << endl;
-        r.extract_json().then([](json::value v){
-            auto i = v.as_object().at(U("order")).as_array();
-            for(auto I : i)
-                cout<<I.as_string()<<'\n';
-        
-        });   // 동기는 get(), 비동기는 then() 사용
-        
-		// r.extract_json(true).then([](json::value v) {
-		// 	cout << v.at(U("store_id")).as_integer() << endl;
-		// 	cout << v.at(U("table_num")).as_integer() << endl;
-        //     cout << v.at(U("orderer")).as_string() << endl;
-        //     auto order = v.at(U("order")).as_array();
-        //     for(auto i : order)
-        //         cout<<i<<'\n';
-		// }).wait();
- 
-	}).wait();
- 
+			cout << U("STATUS : ") << r.status_code() << endl;
+			cout << "content-type : " << r.headers().content_type() << endl;
+			r.extract_json().then([](json::value v){
+					auto i = v.as_object().at(U("order")).as_array();
+					for(auto I : i)
+					cout<<I.as_string()<<'\n';
+
+					});   // 동기는 get(), 비동기는 then() 사용
+
+			// r.extract_json(true).then([](json::value v) {
+			// 	cout << v.at(U("store_id")).as_integer() << endl;
+			// 	cout << v.at(U("table_num")).as_integer() << endl;
+			//     cout << v.at(U("orderer")).as_string() << endl;
+			//     auto order = v.at(U("order")).as_array();
+			//     for(auto i : order)
+			//         cout<<i<<'\n';
+			// }).wait();
+
+			}).wait();
+
 }
 
 void RecieveOrderList()
 {
-    websocket_client client;
-    client.connect(U("ws://3.35.95.187:3001")).then([](){});
-    client.receive().then([](websocket_incoming_message msg){
-        return msg.extract_string();
-    }).then([](string body){
-        cout<<body<<endl;
-    });
-    client.close();
+	websocket_client client;
+	client.connect(U("ws://3.35.95.187:3001")).then([](){});
+	client.receive().then([](websocket_incoming_message msg){
+			return msg.extract_string();
+			}).then([](string body){
+				cout<<body<<endl;
+				});
+	client.close();
 }
