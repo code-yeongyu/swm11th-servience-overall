717084ab61eb30fa51e29ce4ff42c88968a04788
YeonGyu Kim <code.yeon.gyu@gmail.com>
2020-10-20 14:48:04 +0900

    Add setups for pagination

--- display/src/my-app/src/index.js
+++ display/src/my-app/src/index.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Root from './client/Root';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <Root />, document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
