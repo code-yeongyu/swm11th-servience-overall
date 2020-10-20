8b0a21bbe27dad0abb78285c6c36306a5011c62f
YeonGyu Kim <code.yeon.gyu@gmail.com>
2020-10-20 14:48:14 +0900

    Add moving page

---
+++ display/src/my-app/src/pages/Moving.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Moving.css'
+
+const Moving = () => {
+    return (
+        <div className="moving full-center">
+            <p>이동중입니다</p>
+        </div>
+    )
+}
+
+export default Moving
