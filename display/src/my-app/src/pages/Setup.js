8f57fe898224f10a04403c9a934cec2d94c1fc5b
YeonGyu Kim <code.yeon.gyu@gmail.com>
2020-10-28 10:26:13 +0900

    Update Setup Page

--- display/src/my-app/src/pages/Setup.js
+++ display/src/my-app/src/pages/Setup.js
@@ -1,16 +1,24 @@
-import React from 'react'
-import {Input, Button, Form, FormGroup, Label} from 'reactstrap'
+import React, {useState} from 'react';
+import { Input, Button, Form, FormGroup, Label } from 'reactstrap'
 import './Setup.css'
 
 const Setup = () => {
+    const [id, setID] = useState("")
+    const handleChange = ({ target: { value } }) => setID(value)
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        localStorage.setItem("robotID", id)
+        window.location.href = "/home"
+    }
+
     return (
         <div className="setup">
-            <Form>
+            <Form autocomplete="off" onSubmit={handleSubmit}>
                 <FormGroup>
                     <Label for="robotID" size="lg">로봇 본체에 붙어있는 id를 입력해주세요.</Label>
-                    <Input id="robotID" placeholder="Robot ID" size="lg"/>
+                    <Input id="robotID" onChange={handleChange} placeholder="Robot ID" size="lg" type/>
                 </FormGroup>
-                <Button color="primary" size="lg">등록</Button>
+                <Button type="submit" color="primary" size="lg">등록</Button>
             </Form>
         </div>
     )
