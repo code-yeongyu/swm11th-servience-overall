2386107f32475b56a1d3da83f6fa672db2fb40d3
YeonGyu Kim <code.yeon.gyu@gmail.com>
2020-10-29 11:00:58 +0900

    Update order component to have the props

--- display/src/my-app/src/components/order.js
+++ display/src/my-app/src/components/order.js
@@ -2,14 +2,14 @@ import React from 'react'
 import { Card, CardBody, CardTitle, CardSubtitle } from 'reactstrap'
 import "./order.css"
 
-const Order = () => {
+const Order = (props) => {
     return (
         <div className="order-card">
             <Card>
                 <CardBody>
-                    <CardTitle><h2>1번 테이블 - 김성수</h2></CardTitle>
+                    <CardTitle><h2>테이블 {props.tableNumber} - {props.name}</h2></CardTitle>
                     <br/>
-                    <CardSubtitle><h4>아메리카노, 카페라떼</h4></CardSubtitle>
+                    <CardSubtitle><h4>{props.menu}</h4></CardSubtitle>
                 </CardBody>
             </Card>
         </div>
